65d6efb7fc874c8d7ad40caab8a91d106188a5da
sqlda <admin@sqlda.com>
2017-08-29 09:19:45 -0700

    added decision drone code
    needed to add bot service code

--- DroneWebServer.js
+++ DroneWebServer.js
@@ -37,7 +37,7 @@ app.get('/', function (req, res) {
 // to take off
 app.get('/takeoff', function(req, res) {
   client.takeoff();
-  console.log("Barth Joe Drone Taking Off");
+  console.log("Barth Drone Taking Off");
 });
 
 // This router is sending a command to the drone
